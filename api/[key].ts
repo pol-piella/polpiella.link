45537aa82387708423aed468106f4ca2bc4188d5
Pol Piella Abadia <hi@polpiella.dev>
2022-08-11 00:11:35 +0200

    Turns serverless into edge function

--- api/[key].ts
+++ api/[key].ts
@@ -1,6 +1,10 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Redis } from '@upstash/redis'
 
+export const config = {
+  runtime: 'experimental-edge',
+};
+
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL as string,
   token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
