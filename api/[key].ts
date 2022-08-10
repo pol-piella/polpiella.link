b8437ecbebaa1784810d17776f28bce9afac9f83
Pol Piella Abadia <hi@polpiella.dev>
2022-08-10 23:33:36 +0200

    Updates tokens

--- api/[key].ts
+++ api/[key].ts
@@ -5,11 +5,9 @@ import { Redis } from '@upstash/redis'
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { key } = req.query
 
-  console.log(key)
-
   const redis = new Redis({
-    url: 'https://global-concrete-satyr-31044.upstash.io',
-    token: 'AnlEASQgYzE0MzliOGUtNWFhNy00ZTFjLTgzYTEtZWRiNThkM2I4N2Vmx2YFZOSQ46v1Yn4-9nNlg6utv01LyVSrIxt8mWxoHuQ=',
+    url: process.env.UPSTASH_REDIS_REST_URL as string,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
   })
 
   const data = await redis.get(key as string);
