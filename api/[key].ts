f03ed8c55e4b6f056d198dec68b73b4e697f7e7a
Pol Piella Abadia <hi@polpiella.dev>
2022-08-10 23:47:32 +0200

    Moves redis init outside of the function

--- api/[key].ts
+++ api/[key].ts
@@ -1,17 +1,14 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Redis } from '@upstash/redis'
 
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL as string,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
+})
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { key } = req.query
+  const url = await redis.get(key as string);
 
-  const redis = new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL as string,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
-  })
-
-  const data = await redis.get(key as string);
-  console.log(data)
-
-  res.send('hello');
+  res.status(200).send({ url });
 };
