c16ac5dc856dad49f4624b3e34264dc884833414
Pol Piella Abadia <hi@polpiella.dev>
2022-08-10 23:25:24 +0200

    Adds correct key handling

--- api/[key].ts
+++ api/[key].ts
@@ -3,16 +3,16 @@ import { Redis } from '@upstash/redis'
 
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  console.log(req.query)
+  const { key } = req.query
 
-  const key = req.query.key as string
+  console.log(key)
 
   const redis = new Redis({
     url: 'https://global-concrete-satyr-31044.upstash.io',
     token: 'AnlEASQgYzE0MzliOGUtNWFhNy00ZTFjLTgzYTEtZWRiNThkM2I4N2Vmx2YFZOSQ46v1Yn4-9nNlg6utv01LyVSrIxt8mWxoHuQ=',
   })
 
-  const data = await redis.get(key);
+  const data = await redis.get(key as string);
   console.log(data)
 
   res.status(200).send(key);
