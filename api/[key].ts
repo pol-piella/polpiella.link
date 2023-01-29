d71ad197c3080f505b992b86a880efa232055fcd
Pol Piella Abadia <hi@polpiella.dev>
2023-01-29 17:01:47 +0000

    Format

--- api/[key].ts
+++ api/[key].ts
@@ -1,32 +1,32 @@
-import { Redis } from "@upstash/redis";
+import { Redis } from '@upstash/redis'
 
 export const config = {
-  runtime: "edge",
-};
+  runtime: 'edge',
+}
 
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL as string,
   token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
-});
+})
 
 export default async function handler(req: Request) {
-  const searchParams = new URL(req.url).searchParams;
-  const key = searchParams.get("key");
+  const searchParams = new URL(req.url).searchParams
+  const key = searchParams.get('key')
 
   try {
     if (!key) {
-      return new Response("Missing argument `key`", { status: 400 });
+      return new Response('Missing argument `key`', { status: 400 })
     }
 
-    const url: string | null = await redis.get(key);
+    const url: string | null = await redis.get(key)
 
     if (!url)
       return new Response(`Could not find a url with key ${key}`, {
         status: 404,
-      });
+      })
 
-    return Response.redirect(url);
+    return Response.redirect(url)
   } catch (e) {
-    return new Response(JSON.stringify({ error: e }), { status: 500 });
+    return new Response(JSON.stringify({ error: e }), { status: 500 })
   }
 }
