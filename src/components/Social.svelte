698d2e222f6604170d4e1c30b8a4835ba306f8b1
Pol Piella Abadia <hi@polpiella.dev>
2023-02-06 22:22:46 +0000

    Makes button text color white

--- src/components/Social.svelte
+++ src/components/Social.svelte
@@ -9,6 +9,6 @@
 <div class={`${background} py-3 hover:bg-opacity-80 transition-colors`}>
   <a href={url} class="w-full flex justify-center items-center gap-3">
     <img class="aspect-square w-6" src={icon} />
-    <p class={`text-md text-${text}`}>{title}</p>
+    <p class="text-md text-white">{title}</p>
   </a>
 </div>
