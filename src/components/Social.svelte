9f384ffe39a40313aa7b6968efc74d51c2302488
Pol Piella Abadia <hi@polpiella.dev>
2023-02-06 22:20:37 +0000

    Creates a new social component

---
+++ src/components/Social.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let url: string;
+  export let icon: string;
+  export let title: string;
+  export let background: string;
+  export let text: string;
+</script>
+
+<div class={`${background} py-3 hover:bg-opacity-80 transition-colors`}>
+  <a href={url} class="w-full flex justify-center items-center gap-3">
+    <img class="aspect-square w-6" src={icon} />
+    <p class={`text-md text-${text}`}>{title}</p>
+  </a>
+</div>
