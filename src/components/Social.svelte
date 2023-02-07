3e5d45a8980d4ab1c56a2b5b2319a84522d1ffcf
Pol Piella Abadia <pol.piellaabadia@bbc.co.uk>
2023-02-07 09:44:13 +0000

    Adds tailwind prose

--- src/components/Social.svelte
+++ src/components/Social.svelte
@@ -3,12 +3,11 @@
   export let icon: string;
   export let title: string;
   export let background: string;
-  export let text: string;
 </script>
 
-<div class={`${background} py-3 hover:bg-opacity-80 transition-colors`}>
+<div class={`${background} py-3 hover:bg-opacity-80 transition-colors not-prose`}>
   <a href={url} class="w-full flex justify-center items-center gap-3">
-    <img class="aspect-square w-6" src={icon} />
-    <p class="text-md text-white">{title}</p>
+    <img class="aspect-square w-6" src={icon} alt={`Social media icon for ${title}`} />
+    <p class="text-white">{title}</p>
   </a>
 </div>
